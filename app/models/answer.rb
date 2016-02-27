b47a11d7b037531f9271b50c0bd837bf844858b7
Andrey Khataev <khataev@gmail.com>
2016-02-27 17:53:53 +0300

    Added Attachable concern

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,14 +1,13 @@
 # Model for Answer
 class Answer < ActiveRecord::Base
-  has_many :attachments, dependent: :destroy, as: :attachable
+  include Attachable
+
   belongs_to :question
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
   validates :body, presence: true, length: { minimum: 10 }
   validates :question_id, presence: true
   validates :user_id, presence: true
-
-  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 
   default_scope { order(best: :desc, created_at: :asc) }
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,12 +1,11 @@
 # Model for Question
 class Question < ActiveRecord::Base
+  include Attachable
+
   has_many :answers, dependent: :destroy
-  has_many :attachments, dependent: :destroy, as: :attachable
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :body, presence: true, length: { minimum: 10 }
   validates :user_id, presence: true
-
-  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 end
