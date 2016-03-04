db12b7936a4a9ae19cd969e511db05bd7bb27914
Andrey Khataev <khataev@gmail.com>
2016-03-04 19:41:36 +0300

    Extracted partial for votable

---
+++ app/helpers/votable_helper.rb
@@ -0,0 +1,17 @@
+# Helper for Votable controller
+module VotableHelper
+  def vote_for_votable_path(votable)
+    # binding.pry
+    polymorphic_path(votable, action: 'vote_for')
+    # url_for(id: votable, action: 'vote_for')
+    # url_for(controller: controller_name, action: 'vote_for', id: votable)
+  end
+
+  def vote_against_votable_path(votable)
+    polymorphic_path(votable, action: 'vote_against')
+  end
+
+  def vote_back_votable_path(votable)
+    polymorphic_path(votable, action: 'vote_back')
+  end
+end
