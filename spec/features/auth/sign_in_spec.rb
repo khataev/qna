4d65f2b6e69624580a2caa620c4e4cb46f7eea9e
Andrey Khataev <khataev@gmail.com>
2016-05-12 08:12:39 +0300

    Update specs after devise upgrade

--- spec/features/auth/sign_in_spec.rb
+++ spec/features/auth/sign_in_spec.rb
@@ -20,7 +20,7 @@ feature 'User signs in', '
     fill_in 'Password', with: 'wrongpassword'
     click_on 'Log in'
 
-    expect(page).to have_content 'Invalid email or password.'
+    expect(page).to have_content 'Invalid Email or password.'
     expect(current_path).to eq new_user_session_path
   end
 end
