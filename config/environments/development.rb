18afe2b94959e648215cb43f5ff0bc715316df09
Andrey Khataev <khataev@gmail.com>
2016-09-28 09:29:39 +0300

    Temporarily turned off Bullet in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   # Bullet's configuration
   config.after_initialize do
-    Bullet.enable = true
+    Bullet.enable = false
     Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
