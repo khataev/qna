37ec69949745f286cff634b7ff96369d0193022b
Andrey Khataev <khataev@gmail.com>
2016-10-01 20:18:34 +0300

    Beautify attachments delete icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
   def author_of?(obj)
     current_user && current_user.author_of?(obj)
   end
+
+  def delete_attachment_icon
+    if Rails.env.test?
+      'Delete'
+    else
+      "<span class='glyphicon glyphicon-remove'/>".html_safe
+    end
+  end
 end
